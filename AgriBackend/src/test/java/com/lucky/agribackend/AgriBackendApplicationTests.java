6d0760cfc4a11acdb943347290d0ae1ec1cb7833
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-08-09 20:50:09 +0530

    Initial commit Jay Shri Ram

---
+++ AgriBackend/src/test/java/com/lucky/agribackend/AgriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.agribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
