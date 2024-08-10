f507bb973caf7f44713a5dc65b851c0f183f881c
Kunal Bhat <kunalbhat48@gmail.com>
2024-08-10 17:33:37 +0530

    modified crops services and diseases services

--- AgriBackend/src/main/java/com/lucky/agribackend/Dao/DiseaseInterface.java
+++ AgriBackend/src/main/java/com/lucky/agribackend/Dao/DiseaseInterface.java
@@ -1,7 +1,13 @@
 package com.lucky.agribackend.Dao;
 
+import com.lucky.agribackend.entity.Crop;
 import com.lucky.agribackend.entity.Disease;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiseaseInterface extends JpaRepository<Disease,Integer> {
+        public List<Disease> findByName(String name);
+
 }
--- AgriBackend/src/main/java/com/lucky/agribackend/Service/DiseaseService.java
+++ AgriBackend/src/main/java/com/lucky/agribackend/Service/DiseaseService.java
@@ -1,19 +1,37 @@
 package com.lucky.agribackend.Service;
 
+import com.lucky.agribackend.Dao.DiseaseInterface;
 import com.lucky.agribackend.entity.Disease;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DiseaseService {
+
+    @Autowired
+    private DiseaseInterface DiseaseRepo;
+
     public List<Disease> searchDiseases(String keyword) {
+        return DiseaseRepo.findByName(keyword);
     }
 
     public Disease createDisease(Disease disease) {
+        return DiseaseRepo.save(disease);
     }
 
     public Disease updateDisease(Disease disease) {
+        Disease existingDisease = DiseaseRepo.findById(disease.getId()).orElse(null);
+        if (existingDisease != null) {
+            existingDisease.setName(disease.getName());
+            existingDisease.setType(disease.getType());
+        } else {
+            return null;
+        }
+        return DiseaseRepo.save(existingDisease);
     }
 
     public void deleteDisease(int id) {
+        DiseaseRepo.deleteById(id);
     }
 }
